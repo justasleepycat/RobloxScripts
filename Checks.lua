6b74c6691247625070ce4ecad9651eb4d2894a18
SleepyCatto <justasleepycat2008@gmail.com>
2024-12-15 14:18:52 +0100

    Updated

--- Checks.lua
+++ Checks.lua
@@ -1,3 +1,4 @@
+-- local Checks = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/RobloxScripts/refs/heads/main/Checks.lua"))()
 local Checks = {}
 
 local Players = game:GetService("Players")
@@ -15,11 +16,11 @@ function Checks:IsVisible(startPos : Vector3, endPos : Vector3, MaxDistance : nu
     return result
 end
 
-function Checks:GetClosestPlayerToMouse()
+function Checks:GetClosestPlayerToMouse(config)
     local CurrentCamera = Workspace.CurrentCamera
     local Mouse = Players.LocalPlayer:GetMouse()
 
-    local closestPlayer, maxDist = nil, math.huge
+    local closestPlayer, maxDist = nil, config.MaxDistance or math.huge
     for i,v in pairs(Players:GetChildren()) do
 		pcall(function()
 			if v == Players.LocalPlayer then
@@ -28,10 +29,12 @@ function Checks:GetClosestPlayerToMouse()
 			local Position = v.Character.Head:GetPivot().Position or v.Character:GetPivot().Position
             if Position then
 				local Pos2D, OnScreen = CurrentCamera:WorldToViewportPoint(Position)
-                local dist = (Vector2.new(Pos2D.X, Pos2D.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
-                if dist < maxDist then
-					maxDist = dist
-                    closestPlayer = v
+                if OnScreen then
+                    local dist = (Vector2.new(Pos2D.X, Pos2D.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
+                    if dist < maxDist then
+                        maxDist = dist
+                        closestPlayer = v
+                    end
 				end
 			end
 		end)
@@ -41,7 +44,9 @@ end
 
 --[[ Available Functions
     IsVisible(startPos : Vector3, endPos : Vector3, MaxDistance : number,ignore : table) <-- uses raycasting and returns the raycastresult
-    GetClosestPlayerToMouse() <-- returns the player instance not the character
+    GetClosestPlayerToMouse({
+        MaxDistance = 100,
+    }) <-- returns the player instance not the character
 ]]
 if false then
     for i,v in pairs(game.Workspace.Threats:GetChildren()) do
--- Test.lua
+++ Test.lua
@@ -1,27 +1,38 @@
 local HookingService = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/RobloxScripts/refs/heads/main/Hooking.lua"))()
+local Checks = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/RobloxScripts/refs/heads/main/Checks.lua"))()
 
-local RaycastMethods = {
-	RaycastParams.new,
-	Ray.new,
-}
+local Players = game:GetService("Players")
+local Workspace =  game:GetService("Workspace")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
 
-for i,v in pairs(RaycastMethods) do
-	if isfunctionhooked(v) then
-		restorefunction(v)
-	end
-end
-
-for i,v in pairs(RaycastMethods) do
-	local old; old = hookfunction(v, function(...)
-		local script = getcallingscript()
-		local scriptName = tostring(script)
-
-		if string.find(scriptName, "Movement") then
-			local args = {...}
-			
-			return old(unpack(args))
+local env = require(game:GetService("ReplicatedStorage").Module.RayCast)
+for i,v in pairs(env) do
+	getgenv()[i] = getgenv()[i] ~= nil and getgenv()[i] or env[i]
+	env[i] = function(...)
+		local args = {...}
+		local name = tostring(args[4])
+		if name == Players.LocalPlayer.Name then
+		else
+			local fenv = getfenv(3)
+			if string.find(tostring(fenv.script), "Local") or string.find(tostring(fenv.script), "Alex") or string.find(tostring(fenv.script), "Item") or string.find(tostring(fenv.script), "Tank") or string.find(tostring(fenv.script), "Wheel") or string.find(tostring(fenv.script), "RobberyCargo") then
+			else
+				--print(fenv.script)
+				--for i,v in pairs(args) do
+				--	print(i,v, typeof(v))
+				--end
+				if tostring(getfenv(2).script) == "BulletEmitter" or tostring(getfenv(3).script) == "Taser" then
+					local closestPlr = Checks:GetClosestPlayerToMouse({
+						MaxDistance = 200
+					})
+					if closestPlr then
+						local direction = (closestPlr.Character.Head.CFrame.Position - Workspace.CurrentCamera.CFrame.Position)
+						local dist = direction.Magnitude
+						args[2] = direction.Unit
+					end
+				end
+			end
 		end
-
-		return old(...)
-	end)
+		return getgenv()[i](unpack(args))
+	end
 end
