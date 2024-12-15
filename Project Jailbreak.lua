7e3072c378f817173d4a068816c2ec072157f9ca
SleepyCatto <justasleepycat2008@gmail.com>
2024-12-15 16:48:25 +0100

    Update Project Jailbreak.lua

--- Project Jailbreak.lua
+++ Project Jailbreak.lua
@@ -11,6 +11,7 @@ getgenv().RemoteArgs = getgenv().RemoteArgs ~= nil and getgenv().RemoteArgs or {
     Debug = true,
     DefaultRemote = nil,
 	DamageVehicle = {},
+    TasePlayer = {},
 }
 
 do  -- Block error analytics 
@@ -43,77 +44,102 @@ local function DamageVehicle(vehicle : Instance)
 end
 
 if #getgenv().RemoteArgs.DamageVehicle == 0 then
-    local old; old = hookfunction(HookingService.methodHooks.RemoteEvent, function(...)
-        local Callstack = getcallstack()
-        local args = {...}
 
-        if #Callstack > 5 then
-            if getgenv().RemoteArgs.Debug and getinfo(Callstack[5]).name ~= "UpdateMousePosition" then
-                print("---------------------------------------------------------------------------------------------------")
-                print("Script: ", getcallingscript())
-                print("Args:")
-                print("{")
-                for i,v in pairs(args) do
-                    print("	", i, v, typeof(v))
-                end
-                print("}")
-                print("Function: ", getinfo(Callstack[5]).name)
+end
+local old; old = hookfunction(HookingService.methodHooks.RemoteEvent, function(...)
+    local Callstack = getcallstack()
+    local args = {...}
+
+    if #Callstack > 5 then
+        local FuncName = getinfo(Callstack[5]).name
+        if getgenv().RemoteArgs.Debug and FuncName ~= "UpdateMousePosition" then
+            print("---------------------------------------------------------------------------------------------------")
+            print("Script: ", getcallingscript())
+            print("Args:")
+            print("{")
+            for i,v in pairs(args) do
+                print("	", i, v, typeof(v))
             end
-            pcall(function()
-                getgenv().RemoteArgs.DefaultRemote = old
-                if tostring(getfenv(3).script) then
-                    if typeof(args[3]) == "Instance" and typeof(args[4]) == "string" then
-                        print("DamageVehicle")
-                        getgenv().RemoteArgs.DamageVehicle = args
-                    end
-                end
-            end)
-
-            --for i,v in pairs(Callstack) do
-            --	print(i,v, getinfo(v).name)
-            --end
+            print("}")
+            print("Function: ", FuncName)
         end
-        return old(...)
+        pcall(function()
+            getgenv().RemoteArgs.DefaultRemote = old
+            if FuncName == "Tase" then
+				warn("Tase found")
+				getgenv().RemoteArgs.TasePlayer = args
+			end
+            if tostring(getfenv(3).script) then
+                if typeof(args[3]) == "Instance" and typeof(args[4]) == "string" then
+                    warn("DamageVehicle found")
+                    getgenv().RemoteArgs.DamageVehicle = args
+                end
+            end
+        end)
+        --for i,v in pairs(Callstack) do
+        --	print(i,v, getinfo(v).name)
+        --end
+    end
+    return old(...)
+end)
+
+Players.LocalPlayer:GetMouse().Button1Down:Connect(function()
+    local ClosestPlr = Checks:GetClosestPlayerToMouse({
+        MaxDistance = 300,
+    })
+    if ClosestPlr then
+        if getgenv().RemoteArgs.DefaultRemote == nil then return end
+        task.spawn(function()
+            local args = table.clone(getgenv().RemoteArgs.TasePlayer)
+            args[3] = ClosestPlr.Character:FindFirstChildWhichIsA("Humanoid", true)
+            args[4] = ClosestPlr.Character:FindFirstChild("HumanoidRootPart", true)
+            args[5] = ClosestPlr.Character:FindFirstChild("HumanoidRootPart", true).CFrame.Position
+            warn("Tazing "..ClosestPlr.Name)
+            getgenv().RemoteArgs.DefaultRemote(unpack(args))
+        end)
+	end
+end)
+
+do  -- Vehicle Damage Aura
+    task.spawn(function()
+        while #getgenv().RemoteArgs.DamageVehicle == 0 and task.wait() do end
+        while task.wait(.1) do
+            for i,v in pairs(Workspace.Vehicles:GetChildren()) do
+                pcall(function()
+                    local dist = (v:GetPivot().Position - Workspace.CurrentCamera.CFrame.Position).Magnitude
+                    if dist < 300 then
+                    
+                    end
+                    DamageVehicle(v)
+                end)
+            end
+        end
     end)
 end
 
-while #getgenv().RemoteArgs.DamageVehicle == 0 and task.wait() do end
-task.spawn(function()
-    while task.wait(.1) do
-        for i,v in pairs(Workspace.Vehicles:GetChildren()) do
-            pcall(function()
-                local dist = (v:GetPivot().Position - Workspace.CurrentCamera.CFrame.Position).Magnitude
-                if dist < 300 then
-                
+
+
+do  -- Silent Aimbot
+    local env = require(game:GetService("ReplicatedStorage").Module.RayCast)
+    for i,v in pairs(env) do
+        getgenv()[i] = getgenv()[i] ~= nil and getgenv()[i] or env[i]
+        env[i] = function(...)
+            local args = {...}
+            local name = tostring(args[4])
+            if name == Players.LocalPlayer.Name then
+            else
+                if tostring(getfenv(2).script) == "BulletEmitter" or tostring(getfenv(3).script) == "Taser" then
+                    local closestPlr = Checks:GetClosestPlayerToMouse({
+                        MaxDistance = 400
+                    })
+                    if closestPlr then
+                        local direction = (closestPlr.Character.Head.CFrame.Position - Workspace.CurrentCamera.CFrame.Position)
+                        local dist = direction.Magnitude
+                        args[2] = direction.Unit
+                    end
                 end
-                DamageVehicle(v)
-            end)
+            end
+            return getgenv()[i](unpack(args))
         end
     end
-end)
-
-
-
--- Silent Aimbot
-local env = require(game:GetService("ReplicatedStorage").Module.RayCast)
-for i,v in pairs(env) do
-	getgenv()[i] = getgenv()[i] ~= nil and getgenv()[i] or env[i]
-	env[i] = function(...)
-		local args = {...}
-		local name = tostring(args[4])
-		if name == Players.LocalPlayer.Name then
-		else
-			if tostring(getfenv(2).script) == "BulletEmitter" or tostring(getfenv(3).script) == "Taser" then
-				local closestPlr = Checks:GetClosestPlayerToMouse({
-					MaxDistance = 400
-				})
-				if closestPlr then
-					local direction = (closestPlr.Character.Head.CFrame.Position - Workspace.CurrentCamera.CFrame.Position)
-					local dist = direction.Magnitude
-					args[2] = direction.Unit
-				end
-			end
-		end
-		return getgenv()[i](unpack(args))
-	end
 end
