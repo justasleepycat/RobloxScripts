aea98081231d3a3a24f2e8d26a0d57e0d4debbdd
SleepyCatto <justasleepycat2008@gmail.com>
2024-12-15 11:11:55 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,12 +1,7 @@
--- local HookingService = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/RobloxScripts/refs/heads/main/Hooking.lua"))()
+local HookingService = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/RobloxScripts/refs/heads/main/Hooking.lua"))()
 
 
 HookingService:NameCallSpoof("FireServer", false, function(namecall, self, ...)
-    if string.find(tostring(self), "Primary") then
-        for i = 1,30 do
-            namecall(self, ...)
-            task.wait(.01)
-        end
-    end
+	print(namecall, self)
     return namecall(self, ...)
 end)
