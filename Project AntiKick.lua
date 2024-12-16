510eb9d0ea7c8aefe4e17f4a83e1a55dbbad5ba7
SleepyCatto <justasleepycat2008@gmail.com>
2024-12-16 17:04:05 +0100

    Removed files

--- Project AntiKick.lua
+++
@@ -1,5 +0,0 @@
-local HookingService = loadstring(game:HttpGet("https://raw.githubusercontent.com/justasleepycat/Roblox/refs/heads/dev/Hooks.lua"))()
-
-HookingService:NameCallSpoof("Kick", false, function(namecall, self, ...)
-    return
-end)
