df6e4d669cdcfdb6d3169314765a9c6ab3e603bf
SleepyCatto <justasleepycat2008@gmail.com>
2025-01-02 13:48:53 +0100

    Update ArrayFieldRewrite.lua

--- ArrayFieldRewrite.lua
+++ ArrayFieldRewrite.lua
@@ -676,16 +676,16 @@ function ArrayFieldLibrary:Notify(NotificationSettings)
 		Notification.Name = NotificationSettings.Title or "Unknown Title"
 		Notification.Visible = true
 
-		local blurlight = nil
-		if not false then
-			blurlight = Instance.new("DepthOfFieldEffect",game:GetService("Lighting"))
-			blurlight.Enabled = true
-			blurlight.FarIntensity = 0
-			blurlight.FocusDistance = 51.6
-			blurlight.InFocusRadius = 50
-			blurlight.NearIntensity = 1
-			game:GetService("Debris"):AddItem(script,0)
-		end
+		--local blurlight = nil
+		--if not false then
+		--	blurlight = Instance.new("DepthOfFieldEffect",game:GetService("Lighting"))
+		--	blurlight.Enabled = true
+		--	blurlight.FarIntensity = 0
+		--	blurlight.FocusDistance = 51.6
+		--	blurlight.InFocusRadius = 50
+		--	blurlight.NearIntensity = 1
+		--	game:GetService("Debris"):AddItem(script,0)
+		--end
 
 		Notification.Actions.Template.Visible = false
 
@@ -807,7 +807,7 @@ function ArrayFieldLibrary:Notify(NotificationSettings)
 		wait(0.2)
 		if not false then
 			neon:UnbindFrame(Notification.BlurModule)
-			blurlight:Destroy()
+			--blurlight:Destroy()
 		end
 		wait(0.9)
 		Notification:Destroy()
